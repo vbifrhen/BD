a7c5deb50d0337c9dbcedf9952792f97be0dc3ab
Michael Zemlyanskiy <vbifrhenyfdctulf@mail.ru>
2023-09-16 13:37:26 +0300

    Добавлены подпункты

--- main.cpp
+++ main.cpp
@@ -18,31 +18,51 @@ int main() {
         cout << "Ввод БД:\n";
         cout << "  1.из файла\n";
         cout << "  2.с клавиатуры\n";
+        cin >> meniu;
+        if (meniu == 1) {
+        }
+        if (meniu == 2) {
+          ;
+        }
       }
-      if (meniu == 2) {
+      elif (meniu == 2) {
         ;
       }
-      if (meniu == 3) {
+      elif (meniu == 3) {
         cout << "Редактирование данных:\n";
         cout << "  1.очистить БД\n";
         cout << "  2.добавить запись\n";
         cout << "  3.удалить запись\n";
+        cin >> meniu;
+        if (meniu == 1) {
+        }
+        if (meniu == 2) {
+          ;
+        }
+        if (meniu == 3) {
+        }
       }
-      if (meniu == 4) {
+      elif (meniu == 4) {
         cout << "Вывод БД:\n";
         cout << "  1.в файл\n";
         cout << "  2.на экран\n";
+        cin >> meniu;
+        if (meniu == 1) {
+        }
+        if (meniu == 2) {
+          ;
+        }
       }
-      if (meniu == 5) {
+      elif (meniu == 5) {
         ;
       }
-      if (meniu == 6) {
+      elif (meniu == 6) {
         ;
       }
-      if (meniu == 7) {
+      elif (meniu == 7) {
         ;
       }
-      if (meniu == 8) {
+      elif (meniu == 8) {
         system("cls");
         cout << " МЕНЮ:\n";
         cout << "  1.Ввод БД:\n";
@@ -55,13 +75,12 @@ int main() {
         cout << "  8.Очистить экран\n";
         cout << "  9.Выход\n\n";
       }
-      if (meniu == 9) {
+      elif (meniu == 9) {
         break;
       }
     } else {
       cout << "ERROR! \n\n";
     }
-    cout << "меню: ";
     cin >> meniu;
   }
   system("pause");
